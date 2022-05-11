a76c362c54e0b1c7a27e47f4d5e8870bf5132f66
Beau Elliott <belliott1516@gmail.com>
2022-05-11 16:46:19 -0400

    created getSomething function and getsomethingbyid function in fetch utils

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,7 +1,19 @@
 import { client } from 'client.js';
 
 export default async function getSomething(){
-  const response = await client
+  const { body } = await client
     .from()
     .select();
+
+  return body;
+}
+
+export async function getSomethingByID(id){
+  const response = await client
+    .from()
+    .select()
+    .match({ id })
+    .single();
+
+  return response. body;
 }
