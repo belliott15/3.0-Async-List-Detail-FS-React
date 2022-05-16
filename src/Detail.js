048e8a83d87fcde72e949503c87d5fd16d49e1c7
Beau Elliott <belliott1516@gmail.com>
2022-05-16 09:04:27 -0700

    edited backgrounds for multiple different types

--- src/Detail.js
+++ src/Detail.js
@@ -1,7 +1,28 @@
+import { getByPlaceholderText } from '@testing-library/react';
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getPokemonByID } from './services/fetch-utils';
 
 export default function Detail() {
+  const { id } = useParams();
+  const [singlePokemon, setSinglePokemon] = useState({});
+
+  useEffect(() => {
+    async function getOnePokemon(){
+      const currentPokemon = await getPokemonByID(id);
+      setSinglePokemon(currentPokemon);
+    }
+    getOnePokemon();
+  }, [id]);
   return (
-    <div>Detail</div>
+    <div>
+      <h1>{singlePokemon.name}</h1>
+      <h6>HP:{singlePokemon.hp} Attack:{singlePokemon.attack}  Defense:{singlePokemon.defense}</h6>
+      {singlePokemon.type2 ? <h3>Type: {singlePokemon.type1} / {singlePokemon.type2}</h3> 
+        : <h3>Type: {singlePokemon.type1}</h3>}
+      <h3>PokeDex ID: {singlePokemon.id}</h3>
+      {singlePokemon.legendary && <p style={{ background: 'gold', color: 'darkcyan' }}>Legendary</p>}
+    </div>
   );
 }
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -6,7 +6,7 @@ export default function Pokemon({ poke }) {
     <div>
       <Link to={`/detail/${poke.id}`} >
         <div className="poke-card" style={{ 
-          background: poke.legendary && '#a38c0b'
+          background: poke.legendary && '#FFD700'
           || poke.type1 === 'Water' && '#0b8ca3'
           || poke.type1 === 'Fire' && '#d14324'
           || poke.type1 === 'Grass' && '#228520'
@@ -15,7 +15,17 @@ export default function Pokemon({ poke }) {
           || poke.type1 === 'Poison' && '#7f2f8a'
           || poke.type1 === 'Electric' && '#bdb704'
           || poke.type1 === 'Ground' && '#8f6401'
-
+          || poke.type1 === 'Fairy' && '#cf72c2'
+          || poke.type1 === 'Fighting' && '#d43333'
+          || poke.type1 === 'Psychic' && '#852aa8'
+          || poke.type1 === 'Rock' && '#8a745a'
+          || poke.type1 === 'Ghost' && '#645a8a'
+          || poke.type1 === 'Ice' && '#69b9cf'
+          || poke.type1 === 'Dragon' && '#b36f27'
+          || poke.type1 === 'Dark' && '#403f3f'
+          || poke.type1 === 'Steel' && '#b6bac2'
+          || poke.type1 === 'Steele' && '#b6bac2'
+          || poke.type1 === 'Steele' && '#b6bac2'
         }}>
           <h1>{poke.name}</h1>
           {poke.type2 ? <h4><em>Type: </em>{poke.type1} / {poke.type2}</h4>
